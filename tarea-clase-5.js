aa54fd1ddb259f12575928ce424e23d1a904df9c
rubenhernandez1994 <159656794+rubenhernandez1994@users.noreply.github.com>
2024-02-14 21:36:47 -0600

    Tarea clase 5 y main
    Se realiza la tarea clase 5 así como la creación del archivo main.css

---
+++ tarea-clase-5.js
@@ -0,0 +1,21 @@
+//TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+function calcularSalarioMensual(salarioAnual){
+    const MESES_EN_EL_ANIO = 12;
+    return salarioAnual/MESES_EN_EL_ANIO;
+  }
+  
+  const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual')
+  
+  $calcularSalarioMensual.onclick = function(){
+    const salarioAnual = Number(document.querySelector('#salario-anual').value);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+    document.querySelector('#salario-mensual').value = salarioMensual
+  
+    console.log(salarioMensual)
+  
+    return false;
+  }
